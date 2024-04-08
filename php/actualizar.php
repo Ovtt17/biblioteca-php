e6a6b3a913c963999729ab78d03f1b932884a2d1
Ovett Mora <ovettmora17@gmail.com>
2024-04-07 23:00:03 -0600

    second version

--- php/actualizar.php
+++ php/actualizar.php
@@ -53,8 +53,12 @@ $libro = $crud->obtenerLibro($_GET['id']);
                   <input type='text' id="autor" name='autor' class="form-control" value='<?php echo $libro->getAutor() ?>'>
                 </div>
                 <div class="form-group">
-                  <label for="edicion">Fecha Edición:</label>
-                  <input type='text' id="edicion" name='edicion' class="form-control" value='<?php echo $libro->getAnio_edicion() ?>'>
+                  <label for="anio_edicion">Fecha Edición:</label>
+                  <input type='text' id="anio_edicion" name='anio_edicion' class="form-control" value='<?php echo $libro->getAnio_edicion() ?>'>
+                </div>
+                <div class="form-group">
+                  <label for="editorial">Editorial:</label>
+                  <input type='text' id="editorial" name='editorial' class="form-control" value='<?php echo $libro->getEditorial() ?>'>
                 </div>
                 <input type='hidden' name='actualizar' value='actualizar'>
                 <div class="d-flex justify-content-center">
--- php/mostrar.php
+++ php/mostrar.php
@@ -22,6 +22,7 @@ $listaLibros = $crud->mostrar();
       <td>Nombre</td>
       <td>Autor</td>
       <td>Edicion</td>
+      <td>Editorial</td>
       <td>Actualizar</td>
       <td>Eliminar</td>
     </head>
@@ -32,6 +33,7 @@ $listaLibros = $crud->mostrar();
           <td><?php echo $libro->getNombre() ?></td>
           <td><?php echo $libro->getAutor() ?></td>
           <td><?php echo $libro->getAnio_edicion() ?> </td>
+          <td><?php echo $libro->getEditorial() ?> </td>
           <td><a href="actualizar.php?id=<?php echo $libro->getId() ?>&accion=a">Actualizar</a> </td>
           <td><a href="administrar_libro.php?id=<?php echo $libro->getId() ?>&accion=e">Eliminar</a> </td>
         </tr>
